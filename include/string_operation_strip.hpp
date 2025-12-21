09239005a88427ba8700c412486e939cb780a80b
JourneyCodesAyush <mithilesh71170@gmail.com>
2025-12-21 21:33:40 +0530

    feat: add helper function to strip a string

---
+++ include/string_operation_strip.hpp
@@ -0,0 +1,15 @@
+#ifndef cloc_string_operation_strip_hpp
+#define cloc_string_operation_strip_hpp
+
+#include <string>
+
+std::string strip(const std::string &s)
+{
+    size_t start = s.find_first_not_of(" \t\r\n");
+    if (start == std::string::npos)
+        return "";
+    size_t end = s.find_last_not_of(" \t\r\n");
+    return s.substr(start, end - start + 1);
+}
+
+#endif
