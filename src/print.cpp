2208fd68c047078ce2c258a766b38a804bdaa4a2
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-12-22 13:43:44 +0530

    feat(print): print statistics closer to CLOC-style
    - Use dashes (-) instead of pipes (|) to separate statistics per file

--- src/print.cpp
+++ src/print.cpp
@@ -5,32 +5,43 @@
 #include "print.hpp"
 #include "stats.hpp"
 
+static void print_dashes()
+{
+    std::cout << "------------------------------------------------------------------------------";
+}
+
 void print::print_single(stats::Stats &stats)
 {
-    std::cout << std::left << std::setw(12) << stats.file_type
-              << std::right << std::setw(6) << stats.file_count
-              << " | " << std::setw(6) << stats.lines_of_code
-              << " | " << std::setw(8) << stats.lines_of_comment
-              << " | " << std::setw(12) << stats.blank_lines
-              << " | " << std::setw(11) << stats.total()
+    std::cout << std::left << std::setw(20) << stats.file_type
+              << std::right << std::setw(8) << stats.file_count
+              << std::setw(9) << stats.lines_of_code
+              << std::setw(12) << stats.lines_of_comment
+              << std::setw(13) << stats.blank_lines
+              << std::setw(15) << stats.total()
               << "\n";
 }
 
 void print::print_result_map(std::map<detector::FileType, stats::Stats> &statistics, double duration_ms)
 {
-    std::cout << "\nC++ implementation of CLOC\n";
+    std::cout << "\n\nC++ implementation of CLOC\n";
     std::cout << "Total time: " << duration_ms << " seconds\n";
+    print_dashes();
+    std::cout << "\n";
     // Header
-    std::cout << std::left << std::setw(12) << "Language"
-              << std::right << std::setw(6) << "Files"
-              << " | " << std::setw(6) << "Code"
-              << " | " << std::setw(8) << "Comments"
-              << " | " << std::setw(12) << "Blank lines"
-              << " | " << std::setw(11) << "Total lines"
+    std::cout << std::left << std::setw(20) << "Language"
+              << std::right << std::setw(8) << "Files"
+              << std::setw(9) << "Code"
+              << std::setw(12) << "Comments"
+              << std::setw(13) << "Blank"
+              << std::setw(15) << "Total"
               << "\n";
 
     for (auto &pair : statistics)
     {
+        print_dashes();
+        std::cout << "\n";
         print::print_single(pair.second);
     }
+    print_dashes();
+    std::cout << "\n";
 }
