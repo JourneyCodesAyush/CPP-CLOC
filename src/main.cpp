1513bb42be28fa5933fd72eb35a56838031b706e
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-12-22 16:53:29 +0530

    feat(main): handle CLI arguments with argparse
    - Adds positional 'path' argument to either file or directory
    - Catches and prints exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
+#include <argparse/argparse.hpp>
+#include <iostream>
+#include <string>
+#include <exception>
+
+#include "list_files.hpp"
 #include "middleware.hpp"
 
 int main(int argc, char const *argv[])
 {
-    if (argc == 2)
+    argparse::ArgumentParser program("cpp_cloc");
+
+    program.add_argument("path").help("File or directory to analyze");
+
+    try
     {
-        middleware::process_file(argv[1]);
+        program.parse_args(argc, argv);
+
+        std::string path = program.get<std::string>("path");
+        auto files = list_files(path);
+        middleware::process_file(files);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what();
+        return 1;
     }
 
     return 0;
