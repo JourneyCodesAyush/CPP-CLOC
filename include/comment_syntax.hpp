6e3bd41497a289da077f673b41adafeba94bd5cc
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2025-12-22 10:38:36 +0530

    feat(comment-syntax): add CommentSyntax struct with predefined language instances

---
+++ include/comment_syntax.hpp
@@ -0,0 +1,21 @@
+#ifndef cloc_comment_syntax_hpp
+#define cloc_comment_syntax_hpp
+
+#include <string>
+
+namespace comment_syntax
+{
+    struct CommentSyntax
+    {
+        bool supports_multi_line;
+        std::string single_line;
+        std::string multi_line_start;
+        std::string multi_line_end;
+    };
+
+    const CommentSyntax CLikeComments{true, "//", "/*", "*/"};
+    const CommentSyntax PythonComments{true, "#", "\"\"\"", "\"\"\""};
+    const CommentSyntax BashComments{true, "#", "\"\"\"", "\"\"\""};
+}
+
+#endif
