53d01a9a404f69ea1495aeb547830dabeb64ee65
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-02-23 20:58:03 +0530

    refactor: move strip() implementation to cpp to fix ODR violation

--- include/string_operation_strip.hpp
+++ include/string_operation_strip.hpp
@@ -2,11 +2,4 @@
 
 #include <string>
 
-std::string strip(const std::string &s)
-{
-    size_t start = s.find_first_not_of(" \t\r\n");
-    if (start == std::string::npos)
-        return "";
-    size_t end = s.find_last_not_of(" \t\r\n");
-    return s.substr(start, end - start + 1);
-}
+std::string strip(const std::string &s);
---
+++ src/string_operation_strip.cpp
@@ -0,0 +1,10 @@
+#include "string_operation_strip.hpp"
+    
+std::string strip(const std::string &s)
+{
+    size_t start = s.find_first_not_of(" \t\r\n");
+    if (start == std::string::npos)
+        return "";
+    size_t end = s.find_last_not_of(" \t\r\n");
+    return s.substr(start, end - start + 1);
+}
